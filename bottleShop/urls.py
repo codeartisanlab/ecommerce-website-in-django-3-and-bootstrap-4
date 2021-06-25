1da96415bab5a4609b99b0cddbacdfa021769f68
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-06-25 23:28:39 +0530

    part-32-improve-admin-ui-with-django-jet-package

--- bottleShop/urls.py
+++ bottleShop/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('jet/', include('jet.urls')),
+    path('jet/dashboard/', include('jet.dashboard.urls','jet-dashboard')),
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
