43c25e167d10000f904ff55f9186e16f024ed944
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-07-13 22:35:48 +0530

    set the static root

--- bottleShop/settings.py
+++ bottleShop/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
